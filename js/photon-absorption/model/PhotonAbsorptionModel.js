60ddb87b01fbd948c16a50a586eb9bdf47f420d0
jessegreenberg <jesse.greenberg@colorado.edu>
2014-11-18 20:03:15 -0700

    Fix accumulated lint issues, see issue #76

--- js/photon-absorption/model/PhotonAbsorptionModel.js
+++ js/photon-absorption/model/PhotonAbsorptionModel.js
@@ -33,14 +33,12 @@ define( function( require ) {
   var NO2 = require( 'MOLECULES_AND_LIGHT/photon-absorption/model/molecules/NO2' );
   var O2 = require( 'MOLECULES_AND_LIGHT/photon-absorption/model/molecules/O2' );
   var O3 = require( 'MOLECULES_AND_LIGHT/photon-absorption/model/molecules/O3' );
-  var CH4 = require( 'MOLECULES_AND_LIGHT/photon-absorption/model/molecules/CH4' );
   var PhotonTarget = require( 'MOLECULES_AND_LIGHT/photon-absorption/model/PhotonTarget' );
 
   // constants
 
   // Constants that control where and how photons are emitted.
   var PHOTON_EMISSION_LOCATION = new Vector2( -2000, 0 );
-  var PHOTON_EMISSION_ANGLE_RANGE = Math.PI / 2;
 
   // Velocity of emitted photons.  Since they are emitted horizontally, only one value is needed.
   var PHOTON_VELOCITY = 3.0;
