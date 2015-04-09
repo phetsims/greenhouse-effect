0a3457e480e6a84652a8a2abb211d33dcd4166ff
Chris Malley <cmalley@pixelzoom.com>
2015-04-09 11:16:27 -0600

    remove extra space from copyright comment on line 1 of .js files

--- js/photon-absorption/model/Molecule.js
+++ js/photon-absorption/model/Molecule.js
@@ -1,4 +1,4 @@
-//  Copyright 2002-2014, University of Colorado
+// Copyright 2002-2014, University of Colorado
 
 /**
  * A model for a particular molecule.  This, by its nature, is essentially a composition of other objects, generally
