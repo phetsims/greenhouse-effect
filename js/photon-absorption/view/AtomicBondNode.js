57c42f64446b35a1a08809bf232472f507e42c4d
Sam Reid <reids@colorado.edu>
2015-10-06 13:08:06 -0600

    Fixed lint issues, see https://github.com/phetsims/chipper/issues/235

--- js/photon-absorption/view/AtomicBondNode.js
+++ js/photon-absorption/view/AtomicBondNode.js
@@ -107,7 +107,7 @@ define( function( require ) {
           break;
 
         default:
-          console.error( " - Error: Can't represent bond number, value = " + this.atomicBond.getBondCount() );
+          console.error( ' - Error: Can\'t represent bond number, value = ' + this.atomicBond.getBondCount() );
           assert && assert( false );
           break;
       }
@@ -174,7 +174,7 @@ define( function( require ) {
           break;
 
         default:
-          console.error( " - Error: Can't represent bond number, value = " + this.atomicBond.bondCount );
+          console.error( ' - Error: Can\'t represent bond number, value = ' + this.atomicBond.bondCount );
           assert && assert( false );
           break;
 
