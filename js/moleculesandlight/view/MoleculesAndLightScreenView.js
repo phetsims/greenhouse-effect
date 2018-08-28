f60cd2a55352dd954b0262d24855e36e9a6b9ddc
zepumph <michael.kauzmann@colorado.edu>
2018-08-28 15:09:48 -0800

    rename a11yEndListener options --> onAccessibleClick, https://github.com/phetsims/scenery/issues/831

--- js/moleculesandlight/view/MoleculesAndLightScreenView.js
+++ js/moleculesandlight/view/MoleculesAndLightScreenView.js
@@ -201,7 +201,7 @@ define( function( require ) {
         }
         dialog.show();
       },
-      a11yEndListener: function() {
+      onAccessibleClick: function() {
 
         // if (and only if) the dialog is opened with the keyboard, send focus directly to the close button
         assert && assert( dialog, 'No dialog was created, nothing to focus.' );
