c12b2cb49d6e20bbd176635de602945b5415d554
samreid <reids@colorado.edu>
2018-11-19 12:55:06 -0700

    Update copyright dates, see https://github.com/phetsims/chipper/issues/711

--- js/moleculesandlight/view/QuadEmissionFrequencyControlPanel.js
+++ js/moleculesandlight/view/QuadEmissionFrequencyControlPanel.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2017, University of Colorado Boulder
+// Copyright 2014-2018, University of Colorado Boulder
 
 /**
  * This is a control panel that is intended for use in the play area and that allows the setting of 4 different photon
