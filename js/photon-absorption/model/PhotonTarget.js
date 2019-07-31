7c140845765707794c791b06ec23d0163876ace6
samreid <reids@colorado.edu>
2019-07-31 12:23:02 -0600

    Improve PhET-iO support for Enumeration, including documentation and uppercase API, and more verbose type name. see https://github.com/phetsims/axon/issues/255

--- js/photon-absorption/model/PhotonTarget.js
+++ js/photon-absorption/model/PhotonTarget.js
@@ -31,23 +31,25 @@ define( require => {
     'SINGLE_H2O_MOLECULE',
     'SINGLE_NO2_MOLECULE',
     'SINGLE_O3_MOLECULE'
-  ], function( enumeration ) {
+  ], {
+    beforeFreeze: function( enumeration ) {
 
-    /**
-     * maps photon target to translatable string
-     * @param {PhotonTarget} photonTarget
-     * @returns {string} - the control panel molecule name
-     */
-    enumeration.getMoleculeName = function( photonTarget ) {
-      return photonTarget === enumeration.SINGLE_CO_MOLECULE ? controlPanelCarbonMonoxideString :
-             photonTarget === enumeration.SINGLE_N2_MOLECULE ? controlPanelNitrogenString :
-             photonTarget === enumeration.SINGLE_O2_MOLECULE ? controlPanelOxygenString :
-             photonTarget === enumeration.SINGLE_CO2_MOLECULE ? controlPanelCarbonDioxideString :
-             photonTarget === enumeration.SINGLE_NO2_MOLECULE ? controlPanelNitrogenDioxideString :
-             photonTarget === enumeration.SINGLE_H2O_MOLECULE ? controlPanelWaterString :
-             photonTarget === enumeration.SINGLE_O3_MOLECULE ? controlPanelOzoneString :
-             photonTarget === enumeration.SINGLE_CH4_MOLECULE ? controlPanelMethaneString :
-             assert && assert( false, 'unknown' );
-    };
+      /**
+       * maps photon target to translatable string
+       * @param {PhotonTarget} photonTarget
+       * @returns {string} - the control panel molecule name
+       */
+      enumeration.getMoleculeName = function( photonTarget ) {
+        return photonTarget === enumeration.SINGLE_CO_MOLECULE ? controlPanelCarbonMonoxideString :
+               photonTarget === enumeration.SINGLE_N2_MOLECULE ? controlPanelNitrogenString :
+               photonTarget === enumeration.SINGLE_O2_MOLECULE ? controlPanelOxygenString :
+               photonTarget === enumeration.SINGLE_CO2_MOLECULE ? controlPanelCarbonDioxideString :
+               photonTarget === enumeration.SINGLE_NO2_MOLECULE ? controlPanelNitrogenDioxideString :
+               photonTarget === enumeration.SINGLE_H2O_MOLECULE ? controlPanelWaterString :
+               photonTarget === enumeration.SINGLE_O3_MOLECULE ? controlPanelOzoneString :
+               photonTarget === enumeration.SINGLE_CH4_MOLECULE ? controlPanelMethaneString :
+               assert && assert( false, 'unknown' );
+      };
+    }
   } ) );
 } );
