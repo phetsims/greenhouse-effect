482251987b51459c2989e7a40af3797a38d52797
phet-dev <phetadmin@gmail.com>
2020-02-05 03:25:48 -0700

    update copyright dates from daily grunt work

--- js/moleculesandlight/view/ObservationWindow.js
+++ js/moleculesandlight/view/ObservationWindow.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2019, University of Colorado Boulder
+// Copyright 2014-2020, University of Colorado Boulder
 
 /**
  * Window for Molecules And Light which holds the photon emitter, photons, and molecules of the photon absorption model.
