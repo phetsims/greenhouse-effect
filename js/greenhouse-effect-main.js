3b2972279ef5ababfec58cbc0283261bcf55d884
zepumph <michael.kauzmann@colorado.edu>
2020-05-07 12:12:38 -0800

    Convert SimLauncher to es6 class, rename to simLauncher.js, https://github.com/phetsims/joist/issues/630

--- js/greenhouse-effect-main.js
+++ js/greenhouse-effect-main.js
@@ -7,7 +7,7 @@
  */
 
 import Sim from '../../joist/js/Sim.js';
-import SimLauncher from '../../joist/js/SimLauncher.js';
+import simLauncher from '../../joist/js/simLauncher.js';
 import Tandem from '../../tandem/js/Tandem.js';
 import greenhouseEffectStrings from './greenhouseEffectStrings.js';
 import GreenhouseEffectScreen from './greenhouse-effect/GreenhouseEffectScreen.js';
@@ -28,9 +28,9 @@ const simOptions = {
   }
 };
 
-// launch the sim - beware that scenery Image nodes created outside of SimLauncher.launch() will have zero bounds
+// launch the sim - beware that scenery Image nodes created outside of simLauncher.launch() will have zero bounds
 // until the images are fully loaded, see https://github.com/phetsims/coulombs-law/issues/70
-SimLauncher.launch( () => {
+simLauncher.launch( () => {
   const sim = new Sim( greenhouseEffectTitleString, [
     new GreenhouseEffectScreen( Tandem.ROOT.createTandem( 'greenhouseEffectScreen' ) ),
     new WavesScreen( Tandem.ROOT.createTandem( 'wavesScreen' ) )
