462e97f5ab510b96f0f70268dc51394a69260c7c
samreid <reids@colorado.edu>
2020-05-11 09:35:01 -0600

    Flip back and forth for wave 1, see https://github.com/phetsims/greenhouse-effect/issues/15

--- js/waves/WavesModel.js
+++ js/waves/WavesModel.js
@@ -16,7 +16,6 @@ import WaveParameterModel from './WaveParameterModel.js';
 
 // constants
 const GROUND_Y = 510;
-const incomingWaveX = 400;
 
 class WavesModel {
   constructor( options ) {
@@ -38,31 +37,11 @@ class WavesModel {
 
     const toRemove = this.waves.filter( wave => wave.trailingEdgeReachedDestination );
     toRemove.forEach( wave => arrayRemove( this.waves, wave ) );
-
-    // if ( this.yellowWave1Incoming && this.yellowWave1Incoming.almostDone && !this.yellowWave2Incoming ) {
-    //   const sourcePoint = new Vector2( incomingWaveX + 100, 0 );
-    //   const destinationPoint = new Vector2( incomingWaveX + 100, GROUND_Y );
-    //   this.yellowWave2Incoming = new Wave( 'incoming', sourcePoint, destinationPoint, this.yellowWaveParameterModel, 900 );
-    //   this.waves.push( this.yellowWave2Incoming );
-    // }
-    //
-    // if ( this.yellowWave2Incoming && this.yellowWave2Incoming.almostDone && !this.yellowWave1Incoming ) {
-    //   const sourcePoint = new Vector2( incomingWaveX, 0 );
-    //   const destinationPoint = new Vector2( incomingWaveX, GROUND_Y );
-    //   this.yellowWave1Incoming = new Wave( 'incoming', sourcePoint, destinationPoint, this.yellowWaveParameterModel, 900 );
-    //   this.waves.push( this.yellowWave1Incoming );
-    // }
   }
 
-  reset() {
-    this.timeProperty.reset();
-    this.yellowWaveParameterModel.reset();
-    this.redWaveParameterModel.reset();
-
-    this.waves.length = 0;
-
-    const sourcePoint = new Vector2( incomingWaveX, 0 );
-    const destinationPoint = new Vector2( incomingWaveX, GROUND_Y );
+  createIncomingYellowWave( x ) {
+    const sourcePoint = new Vector2( x, 0 );
+    const destinationPoint = new Vector2( x, GROUND_Y );
     const yellowWave1Incoming = new Wave( 'incoming', sourcePoint, destinationPoint, this.yellowWaveParameterModel, 800, {
       onLeadingEdgeReachesTarget: parentWave => {
         const sourcePoint = parentWave.destinationPoint;
@@ -81,10 +60,20 @@ class WavesModel {
       },
 
       onAlmostDone: parentWave => {
-
+        this.createIncomingYellowWave( x === 400 ? 500 : 400 );
       }
     } );
     this.waves.push( yellowWave1Incoming );
+  }
+
+  reset() {
+    this.timeProperty.reset();
+    this.yellowWaveParameterModel.reset();
+    this.redWaveParameterModel.reset();
+
+    this.waves.length = 0;
+
+    this.createIncomingYellowWave( 400 );
   }
 }
 
