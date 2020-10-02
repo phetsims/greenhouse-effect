be005ec54d4db6ae111107173df86ff471f9ec29
pixelzoom <cmalley@pixelzoom.com>
2020-10-01 22:59:07 -0600

    convert Panel subclasses to ES6 class, https://github.com/phetsims/tasks/issues/1044

--- js/moleculesandlight/view/MoleculeSelectionPanel.js
+++ js/moleculesandlight/view/MoleculeSelectionPanel.js
@@ -9,7 +9,6 @@
 
 import Vector2 from '../../../../dot/js/Vector2.js';
 import Shape from '../../../../kite/js/Shape.js';
-import inherit from '../../../../phet-core/js/inherit.js';
 import StringUtils from '../../../../phetcommon/js/util/StringUtils.js';
 import ModelViewTransform2 from '../../../../phetcommon/js/view/ModelViewTransform2.js';
 import PhetFont from '../../../../scenery-phet/js/PhetFont.js';
@@ -18,8 +17,8 @@ import Rectangle from '../../../../scenery/js/nodes/Rectangle.js';
 import RichText from '../../../../scenery/js/nodes/RichText.js';
 import RadioButtonGroup from '../../../../sun/js/buttons/RadioButtonGroup.js';
 import Panel from '../../../../sun/js/Panel.js';
-import moleculesAndLightStrings from '../../moleculesAndLightStrings.js';
 import moleculesAndLight from '../../moleculesAndLight.js';
+import moleculesAndLightStrings from '../../moleculesAndLightStrings.js';
 import CH4 from '../../photon-absorption/model/molecules/CH4.js';
 import CO from '../../photon-absorption/model/molecules/CO.js';
 import CO2 from '../../photon-absorption/model/molecules/CO2.js';
@@ -50,126 +49,126 @@ const MOLECULE_SCALING_FACTOR = 0.0975;
 // the focus highlights are a little larger so they look good in this rounded panel
 const HIGHLIGHT_DILATION = 1.5;
 
-/**
- * Constructor for a Molecules and Light control panel.
- *
- * @param { PhotonAbsorptionModel } model - The model controlled by this panel.
- * @param {Tandem} tandem
- * @constructor
- */
-function MoleculeSelectionPanel( model, tandem ) {
+class MoleculeSelectionPanel extends  Panel {
 
-  let scaleFactor = 1; // Scale factor of the text in this control panel.  Value gets updated as panels are created.
+  /**
+   * @param { PhotonAbsorptionModel } model - The model controlled by this panel.
+   * @param {Tandem} tandem
+   */
+  constructor( model, tandem ) {
 
-  //  Array which holds the formatted text of the control panel.  This will get populated as individual panels are
-  //  created.  Storing the text allows us to call on it later for scaling purposes once the scale factor has been
-  // calculated.
-  const textList = [];
+    let scaleFactor = 1; // Scale factor of the text in this control panel.  Value gets updated as panels are created.
 
-  // Function which creates individual panels of the control panel.  Each panel consists of a molecule name, chemical
-  // formula, and a visual node representing the molecule.
-  // NOTE! As a side-effect, this computes scaleFactor each time it's called.
-  function createRadioButtonContent( moleculeName, moleculeFormula, moleculeNode ) {
+    //  Array which holds the formatted text of the control panel.  This will get populated as individual panels are
+    //  created.  Storing the text allows us to call on it later for scaling purposes once the scale factor has been
+    // calculated.
+    const textList = [];
 
-    // Create a rectangle which holds the molecular name and representing node.  Rectangle enables the proper layout
-    // which is the molecular name aligned to the left of the panel and the molecule node aligned to the right.
-    const backgroundRectangle = new Rectangle( 0, 0, 215, 0 );
+    // Function which creates individual panels of the control panel.  Each panel consists of a molecule name, chemical
+    // formula, and a visual node representing the molecule.
+    // NOTE! As a side-effect, this computes scaleFactor each time it's called.
+    function createRadioButtonContent( moleculeName, moleculeFormula, moleculeNode ) {
 
-    // Create text label for the molecule name.  Use StringUtils to order chemical names and formulas as desired.
-    const moleculeNameString = StringUtils.format( molecularNamePatternString, moleculeName, '<span dir="ltr">' + moleculeFormula + '</span>' );
-    const molecularName = new RichText( moleculeNameString, { fill: 'white', font: new PhetFont( 13 ) } );
-    textList.push( molecularName );
-    molecularName.centerY = backgroundRectangle.centerY;
-    molecularName.left = backgroundRectangle.left + 10;
+      // Create a rectangle which holds the molecular name and representing node.  Rectangle enables the proper layout
+      // which is the molecular name aligned to the left of the panel and the molecule node aligned to the right.
+      const backgroundRectangle = new Rectangle( 0, 0, 215, 0 );
 
-    // Scale the molecule node to an appropriate size for the panel display and set its position in the panel.
-    moleculeNode.scale( MOLECULE_SCALING_FACTOR );
-    moleculeNode.right = backgroundRectangle.right - 10;
-    moleculeNode.centerY = backgroundRectangle.centerY;
+      // Create text label for the molecule name.  Use StringUtils to order chemical names and formulas as desired.
+      const moleculeNameString = StringUtils.format( molecularNamePatternString, moleculeName, '<span dir="ltr">' + moleculeFormula + '</span>' );
+      const molecularName = new RichText( moleculeNameString, { fill: 'white', font: new PhetFont( 13 ) } );
+      textList.push( molecularName );
+      molecularName.centerY = backgroundRectangle.centerY;
+      molecularName.left = backgroundRectangle.left + 10;
 
-    // Determine the scale factor for the text on this panel, primarily for translation.
-    const nameIconDistance = 35; // Minimum distance between the molecule name and node, determined empirically.
-    scaleFactor = Math.min( scaleFactor, ( moleculeNode.left - nameIconDistance ) / molecularName.width );
+      // Scale the molecule node to an appropriate size for the panel display and set its position in the panel.
+      moleculeNode.scale( MOLECULE_SCALING_FACTOR );
+      moleculeNode.right = backgroundRectangle.right - 10;
+      moleculeNode.centerY = backgroundRectangle.centerY;
 
-    // Add the molecular name and molecule node to the selector panel.
-    backgroundRectangle.addChild( molecularName );
-    backgroundRectangle.addChild( moleculeNode );
+      // Determine the scale factor for the text on this panel, primarily for translation.
+      const nameIconDistance = 35; // Minimum distance between the molecule name and node, determined empirically.
+      scaleFactor = Math.min( scaleFactor, ( moleculeNode.left - nameIconDistance ) / molecularName.width );
 
-    return backgroundRectangle;
-  }
+      // Add the molecular name and molecule node to the selector panel.
+      backgroundRectangle.addChild( molecularName );
+      backgroundRectangle.addChild( moleculeNode );
 
-  const createElement = ( photonTarget, formulaString, molecule, tandemName, moleculeNodeOptions ) => {
-    return {
-      node: createRadioButtonContent( PhotonTarget.getMoleculeName( photonTarget ),
-        formulaString, new MoleculeNode( molecule, MODEL_VIEW_TRANSFORM, moleculeNodeOptions ) ),
-      value: photonTarget,
-      tandemName: tandemName,
-      labelContent: createPDOMLabel( molecule )
+      return backgroundRectangle;
+    }
+
+    const createElement = ( photonTarget, formulaString, molecule, tandemName, moleculeNodeOptions ) => {
+      return {
+        node: createRadioButtonContent( PhotonTarget.getMoleculeName( photonTarget ),
+          formulaString, new MoleculeNode( molecule, MODEL_VIEW_TRANSFORM, moleculeNodeOptions ) ),
+        value: photonTarget,
+        tandemName: tandemName,
+        labelContent: createPDOMLabel( molecule )
+      };
     };
-  };
-  const moleculeOptions = { isForIcon: true };
+    const moleculeOptions = { isForIcon: true };
 
-  // Load the radio button content into an array of object literals which holds the node and value for each button.
-  const radioButtonContent = [
-    createElement( PhotonTarget.SINGLE_CO_MOLECULE, MolecularFormulaStrings.CO_FORMULA_STRING, new CO( moleculeOptions ),
-      'singleCOMoleculeRadioButton' ),
-    createElement( PhotonTarget.SINGLE_N2_MOLECULE, MolecularFormulaStrings.N2_FORMULA_STRING, new N2( moleculeOptions ),
-      'singleN2MoleculeRadioButton' ),
-    createElement( PhotonTarget.SINGLE_O2_MOLECULE, MolecularFormulaStrings.O2_FORMULA_STRING, new O2( moleculeOptions ),
-      'singleO2MoleculeRadioButton' ),
-    createElement( PhotonTarget.SINGLE_CO2_MOLECULE, MolecularFormulaStrings.CO2_FORMULA_STRING, new CO2( moleculeOptions ),
-      'singleCO2MoleculeRadioButton' ),
-    createElement( PhotonTarget.SINGLE_CH4_MOLECULE, MolecularFormulaStrings.CH4_FORMULA_STRING, new CH4( moleculeOptions ),
-      'singleCH4MoleculeRadioButton', {
-        scale: 0.88 // scale applied since CH4 is taller than others, empirically determined
-      } ),
-    createElement( PhotonTarget.SINGLE_H2O_MOLECULE, MolecularFormulaStrings.H20_FORMULA_STRING, new H2O( moleculeOptions ),
-      'singleH2OMoleculeRadioButton' ),
-    createElement( PhotonTarget.SINGLE_NO2_MOLECULE, MolecularFormulaStrings.NO2_FORMULA_STRING, new NO2( moleculeOptions ),
-      'singleNO2MoleculeRadioButton' ),
-    createElement( PhotonTarget.SINGLE_O3_MOLECULE, MolecularFormulaStrings.O3_FORMULA_STRING, new O3( moleculeOptions ),
-      'singleO3MoleculeRadioButton' )
-  ];
+    // Load the radio button content into an array of object literals which holds the node and value for each button.
+    const radioButtonContent = [
+      createElement( PhotonTarget.SINGLE_CO_MOLECULE, MolecularFormulaStrings.CO_FORMULA_STRING, new CO( moleculeOptions ),
+        'singleCOMoleculeRadioButton' ),
+      createElement( PhotonTarget.SINGLE_N2_MOLECULE, MolecularFormulaStrings.N2_FORMULA_STRING, new N2( moleculeOptions ),
+        'singleN2MoleculeRadioButton' ),
+      createElement( PhotonTarget.SINGLE_O2_MOLECULE, MolecularFormulaStrings.O2_FORMULA_STRING, new O2( moleculeOptions ),
+        'singleO2MoleculeRadioButton' ),
+      createElement( PhotonTarget.SINGLE_CO2_MOLECULE, MolecularFormulaStrings.CO2_FORMULA_STRING, new CO2( moleculeOptions ),
+        'singleCO2MoleculeRadioButton' ),
+      createElement( PhotonTarget.SINGLE_CH4_MOLECULE, MolecularFormulaStrings.CH4_FORMULA_STRING, new CH4( moleculeOptions ),
+        'singleCH4MoleculeRadioButton', {
+          scale: 0.88 // scale applied since CH4 is taller than others, empirically determined
+        } ),
+      createElement( PhotonTarget.SINGLE_H2O_MOLECULE, MolecularFormulaStrings.H20_FORMULA_STRING, new H2O( moleculeOptions ),
+        'singleH2OMoleculeRadioButton' ),
+      createElement( PhotonTarget.SINGLE_NO2_MOLECULE, MolecularFormulaStrings.NO2_FORMULA_STRING, new NO2( moleculeOptions ),
+        'singleNO2MoleculeRadioButton' ),
+      createElement( PhotonTarget.SINGLE_O3_MOLECULE, MolecularFormulaStrings.O3_FORMULA_STRING, new O3( moleculeOptions ),
+        'singleO3MoleculeRadioButton' )
+    ];
 
-  // If necessary, scale down molecule names by the minimum scale factor.
-  if ( scaleFactor < 1 ) {
-    _.each( textList, function( text ) { text.scale( scaleFactor ); } );
+    // If necessary, scale down molecule names by the minimum scale factor.
+    if ( scaleFactor < 1 ) {
+      _.each( textList, function( text ) { text.scale( scaleFactor ); } );
+    }
+
+    const radioButtons = new RadioButtonGroup( model.photonTargetProperty, radioButtonContent, {
+      spacing: 1.75,
+      baseColor: 'black',
+      buttonContentXMargin: 0,
+      buttonContentYMargin: 1.85,
+      selectedStroke: 'white',
+      deselectedStroke: 'black',
+      deselectedLineWidth: 0,
+      cornerRadius: 7,
+      touchAreaXDilation: 0,
+      touchAreaYDilation: 0,
+      tandem: tandem.createTandem( 'radioButtonGroup' ),
+
+      // pdom
+      a11yHighlightXDilation: HIGHLIGHT_DILATION,
+      a11yHighlightYDilation: HIGHLIGHT_DILATION
+    } );
+
+    // custom group focus highlight so there is enough spacing between button highlight and group highlight
+    const groupCoefficient = FocusHighlightPath.getGroupDilationCoefficient( radioButtons ) + HIGHLIGHT_DILATION;
+    radioButtons.groupFocusHighlight = new FocusHighlightPath( Shape.bounds( radioButtons.bounds.dilated( groupCoefficient ) ), {
+      outerLineWidth: FocusHighlightPath.GROUP_OUTER_LINE_WIDTH,
+      innerLineWidth: FocusHighlightPath.GROUP_INNER_LINE_WIDTH,
+      innerStroke: FocusHighlightPath.FOCUS_COLOR
+    } );
+
+    super( radioButtons, {
+      fill: 'black',
+      tandem: tandem,
+      tagName: 'div',
+      labelTagName: 'h3',
+      labelContent: moleculesString,
+      descriptionContent: moleculesRadioButtonHelpTextString
+    } );
   }
-
-  const radioButtons = new RadioButtonGroup( model.photonTargetProperty, radioButtonContent, {
-    spacing: 1.75,
-    baseColor: 'black',
-    buttonContentXMargin: 0,
-    buttonContentYMargin: 1.85,
-    selectedStroke: 'white',
-    deselectedStroke: 'black',
-    deselectedLineWidth: 0,
-    cornerRadius: 7,
-    touchAreaXDilation: 0,
-    touchAreaYDilation: 0,
-    tandem: tandem.createTandem( 'radioButtonGroup' ),
-
-    // pdom
-    a11yHighlightXDilation: HIGHLIGHT_DILATION,
-    a11yHighlightYDilation: HIGHLIGHT_DILATION
-  } );
-
-  // custom group focus highlight so there is enough spacing between button highlight and group highlight
-  const groupCoefficient = FocusHighlightPath.getGroupDilationCoefficient( radioButtons ) + HIGHLIGHT_DILATION;
-  radioButtons.groupFocusHighlight = new FocusHighlightPath( Shape.bounds( radioButtons.bounds.dilated( groupCoefficient ) ), {
-    outerLineWidth: FocusHighlightPath.GROUP_OUTER_LINE_WIDTH,
-    innerLineWidth: FocusHighlightPath.GROUP_INNER_LINE_WIDTH,
-    innerStroke: FocusHighlightPath.FOCUS_COLOR
-  } );
-
-  Panel.call( this, radioButtons, {
-    fill: 'black',
-    tandem: tandem,
-    tagName: 'div',
-    labelTagName: 'h3',
-    labelContent: moleculesString,
-    descriptionContent: moleculesRadioButtonHelpTextString
-  } );
 }
 
 /**
@@ -187,7 +186,4 @@ function createPDOMLabel( molecule ) {
 }
 
 moleculesAndLight.register( 'MoleculeSelectionPanel', MoleculeSelectionPanel );
-
-inherit( Panel, MoleculeSelectionPanel );
-
 export default MoleculeSelectionPanel;
