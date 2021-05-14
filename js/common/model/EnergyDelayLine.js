fcf9a5d38da1775f391b6ede8b65477fff3b4594
jbphet <john.blanco@colorado.edu>
2021-05-14 16:20:12 -0600

    fixed some problems with the EnergyDelayLine class

--- js/common/model/EnergyDelayLine.js
+++ js/common/model/EnergyDelayLine.js
@@ -39,30 +39,41 @@ class EnergyDelayLine extends EnergySource {
   step( dt ) {
 
     // Put the incoming energy into the delay queue and clear the input.
-    this.delayedEnergyQueue.push( { dt: dt, energy: this.incomingEnergyProperty.value, time: Date.now() } );
+    this.delayedEnergyQueue.push( { dt: dt, energy: this.incomingEnergyProperty.value } );
     this.incomingEnergyProperty.set( 0 );
     this.totalTimeInEnergyQueue += dt;
+    let energyToOutputThisStep = 0;
+
+    // Figure out how much time (and thus energy) to extract from the delay queue.
+    let amountOfTimeToExtract = 0;
+    if ( this.totalTimeInEnergyQueue > this.delayTime ) {
+
+      // We need to extract the same amount as the time step to keep the energy flow steady, NOT just the excess in the
+      // delay queue.
+      amountOfTimeToExtract = dt;
+    }
 
     // Go through the queue and extract the energy that should go out this step.
-    let energyToOutputThisStep = 0;
-    while ( this.totalTimeInEnergyQueue > this.delayTime ) {
+    while ( amountOfTimeToExtract > 0 ) {
 
       // The first entry in the queue is the oldest.  If the incoming dt value indicates that all of this entry should
       // go out this step, remove the entry from the queue and add its energy to the amount to be output this step.
-      if ( dt >= this.delayedEnergyQueue[ 0 ].dt ) {
+      if ( amountOfTimeToExtract >= this.delayedEnergyQueue[ 0 ].dt ) {
         const removedEntry = this.delayedEnergyQueue.shift();
         energyToOutputThisStep += removedEntry.energy;
-        this.totalTimeInEnergyQueue -= this.delayedEnergyQueue[ 0 ].dt;
+        this.totalTimeInEnergyQueue -= removedEntry.dt;
+        amountOfTimeToExtract -= removedEntry.dt;
       }
       else {
 
         // Part of the energy from this entry should go out in this step, and part of it should remain.  Extract the
         // amount of energy based on the energy rate that it represents and the current dt value, and then modify the
         // entry to have the residual time and energy.
-        const proportionToUse = dt / this.delayedEnergyQueue[ 0 ].dt;
+        const proportionToUse = amountOfTimeToExtract / this.delayedEnergyQueue[ 0 ].dt;
         energyToOutputThisStep += this.delayedEnergyQueue[ 0 ].energy * proportionToUse;
         this.delayedEnergyQueue[ 0 ].energy = ( 1 - proportionToUse ) * this.delayedEnergyQueue[ 0 ].energy;
         this.delayedEnergyQueue[ 0 ].dt = ( 1 - proportionToUse ) * this.delayedEnergyQueue[ 0 ].dt;
+        amountOfTimeToExtract = 0;
       }
     }
 
