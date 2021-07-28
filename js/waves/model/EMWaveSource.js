93845bd929f8085647b6eae276a47609655d7343
jbphet <john.blanco@colorado.edu>
2021-07-28 16:31:37 -0600

    shorter inter-wave time, resulted from design meeting

--- js/waves/model/EMWaveSource.js
+++ js/waves/model/EMWaveSource.js
@@ -39,7 +39,7 @@ class EMWaveSource {
       waveIntensityProperty: null,
 
       // {number} - time between waves, in seconds
-      interWaveTime: 1,
+      interWaveTime: 0.5,
 
       // {Range.<number>} - range of lifetimes for this wave, in seconds
       waveLifetimeRange: new Range( 10, 15 )
