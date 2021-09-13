a74ebf43630185f57393293d97a0be5e2806f9b7
Amy Rouinfar <amy.rouinfar@colorado.edu>
2021-09-13 16:41:11 -0600

    initial pass at phetioFeatured for https://github.com/phetsims/greenhouse-effect/issues/75

--- js/greenhouse-effect-phet-io-overrides.js
+++ js/greenhouse-effect-phet-io-overrides.js
@@ -1,2 +1,67 @@
 /* eslint-disable */
-window.phet.preloads.phetio.phetioElementsOverrides = {};
+window.phet.preloads.phetio.phetioElementsOverrides =
+  {
+    "greenhouseEffect.wavesScreen.model.cloudEnabledProperty": {
+      "phetioFeatured": true
+    },
+    "greenhouseEffect.wavesScreen.model.concentrationControlModeProperty": {
+      "phetioFeatured": true
+    },
+    "greenhouseEffect.wavesScreen.model.concentrationProperty": {
+      "phetioFeatured": true
+    },
+    "greenhouseEffect.wavesScreen.model.dateProperty": {
+      "phetioFeatured": true
+    },
+    "greenhouseEffect.wavesScreen.model.energyBalanceVisibleProperty": {
+      "phetioFeatured": true
+    },
+    "greenhouseEffect.wavesScreen.model.isPlayingProperty": {
+      "phetioFeatured": true
+    },
+    "greenhouseEffect.wavesScreen.model.manuallyControlledConcentrationProperty": {
+      "phetioFeatured": true
+    },
+    "greenhouseEffect.wavesScreen.model.outerSpace.incomingUpwardMovingEnergyRateTracker.energyRateProperty": {
+      "phetioFeatured": true
+    },
+    "greenhouseEffect.wavesScreen.model.sunEnergySource.isShiningProperty": {
+      "phetioFeatured": true
+    },
+    "greenhouseEffect.wavesScreen.model.sunEnergySource.outputEnergyRateTracker.energyRateProperty": {
+      "phetioFeatured": true
+    },
+    "greenhouseEffect.wavesScreen.model.surfaceTemperatureKelvinProperty": {
+      "phetioFeatured": true
+    },
+    "greenhouseEffect.wavesScreen.model.surfaceTemperatureVisibleProperty": {
+      "phetioFeatured": true
+    },
+    "greenhouseEffect.wavesScreen.model.surfaceThermometerVisibleProperty": {
+      "phetioFeatured": true
+    },
+    "greenhouseEffect.wavesScreen.model.temperatureUnitsProperty": {
+      "phetioFeatured": true
+    },
+    "greenhouseEffect.wavesScreen.model.timeSpeedProperty": {
+      "phetioFeatured": true
+    },
+    "greenhouseEffect.wavesScreen.view.concentrationControlPanel.concentrationSlider.slider.visibleProperty": {
+      "phetioFeatured": false
+    },
+    "greenhouseEffect.wavesScreen.view.concentrationControlPanel.concentrationSlider.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "greenhouseEffect.wavesScreen.view.concentrationControlPanel.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "greenhouseEffect.wavesScreen.view.observationWindow.instrumentVisibilityControls.energyBalanceCheckbox.visibleProperty": {
+      "phetioFeatured": false
+    },
+    "greenhouseEffect.wavesScreen.view.observationWindow.instrumentVisibilityControls.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "greenhouseEffect.wavesScreen.view.observationWindow.startSunlightButton.visibleProperty": {
+      "phetioFeatured": false
+    }
+  };
