c1d1b99f95c9ce3895c4b754cdf8ff1614795626
jbphet <john.blanco@colorado.edu>
2021-09-15 10:49:54 -0600

    added tandems for clouds in photons screen, see https://github.com/phetsims/greenhouse-effect/issues/79

--- js/photons/model/PhotonsModel.js
+++ js/photons/model/PhotonsModel.js
@@ -34,9 +34,18 @@ class PhotonsModel extends ConcentrationModel {
 
     // Add the clouds.  These are always present in the model, but aren't always visible.  Positions were chosen to
     // look decent.
-    this.clouds.push( new Cloud( new Vector2( -20000, 20000 ), 15000, 3500 ) );
-    this.clouds.push( new Cloud( new Vector2( 5000, 32000 ), 12000, 2500 ) );
-    this.clouds.push( new Cloud( new Vector2( 24000, 25000 ), 18000, 3000 ) );
+    this.clouds.push(
+      new Cloud( new Vector2( -20000, 20000 ), 15000, 3500 ),
+      { tandem: tandem.createTandem( 'cloud0' ) }
+    );
+    this.clouds.push(
+      new Cloud( new Vector2( 5000, 32000 ), 12000, 2500 ),
+      { tandem: tandem.createTandem( 'cloud1' ) }
+    );
+    this.clouds.push(
+      new Cloud( new Vector2( 24000, 25000 ), 18000, 3000 ),
+      { tandem: tandem.createTandem( 'cloud2' ) }
+    );
 
     // @public {NumberProperty} - number of clouds that are active and thus reflecting light
     this.numberOfActiveCloudsProperty = new NumberProperty( 0, {
