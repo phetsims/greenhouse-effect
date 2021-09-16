fe71d78f4b993c1178acd4c6e3f011c140ab0d04
jbphet <john.blanco@colorado.edu>
2021-09-16 11:59:56 -0600

    early shot at the credits, see https://github.com/phetsims/greenhouse-effect/issues/1

--- js/greenhouse-effect-main.js
+++ js/greenhouse-effect-main.js
@@ -21,10 +21,10 @@ phet.greenhouseEffect.mockupOpacityProperty = new NumberProperty( 0 );
 const simOptions = {
   credits: {
     //TODO fill in credits, all of these fields are optional, see joist.CreditsNode
-    leadDesign: '',
+    leadDesign: 'Kathy Perkins, Amy Rouinfar',
     softwareDevelopment: 'John Blanco, Jesse Greenberg, Sam Reid',
-    team: '',
-    qualityAssurance: '',
+    team: 'Wendy Adams, Danielle Harlow, Kelly Lancaster, Trish Loeblein, Robert Parson, Carl Wieman',
+    qualityAssurance: 'Nancy Salpepi, Kathryn Woessner',
     graphicArts: '',
     soundDesign: 'Ashton Morris'
   }
