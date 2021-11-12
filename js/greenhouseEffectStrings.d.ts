3aca534e7843008e557a5b200766cbf804393fc2
Sam Reid <reids@colorado.edu>
2021-11-11 22:37:22 -0700

    Regenerate string file with grunt modulify, see https://github.com/phetsims/chipper/issues/1139

--- js/greenhouseEffectStrings.d.ts
+++
@@ -1,5 +0,0 @@
-// Copyright 2021, University of Colorado Boulder
-
-// type t = { [ key: string ]: string };
-let greenhouseEffectStrings: any;
-export default greenhouseEffectStrings;
---
+++ js/greenhouseEffectStrings.ts
@@ -0,0 +1,318 @@
+// Copyright 2020-2021, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+/* eslint-disable */
+import getStringModule from '../../chipper/js/getStringModule.js';
+import greenhouseEffect from './greenhouseEffect.js';
+
+type StringsType = {
+  'greenhouse-effect': {
+    'title': string
+  },
+  'screen': {
+    'waves': string,
+    'photons': string,
+    'layerModel': string,
+    'micro': string
+  },
+  'surfaceThermometer': string,
+  'showSurfaceTemperature': string,
+  'cloud': string,
+  'startSunlight': string,
+  'energyLegend': {
+    'title': string,
+    'sunlight': string,
+    'infrared': string
+  },
+  'concentrationPanel': {
+    'greenhouseGasConcentration': string,
+    'lots': string,
+    'none': string,
+    'iceAge': string,
+    'waterConcentrationPattern': string,
+    'carbonDioxideConcentrationPattern': string,
+    'methaneConcentrationPattern': string,
+    'nitrousOxideConcentrationPattern': string
+  },
+  'fluxMeter': {
+    'title': string,
+    'energyFlux': string,
+    'sunlight': string,
+    'infrared': string
+  },
+  'energyBalancePanel': {
+    'title': string,
+    'subTitle': string,
+    'in': string,
+    'out': string,
+    'net': string
+  },
+  'temperature': {
+    'units': {
+      'kelvin': string,
+      'celsius': string,
+      'fahrenheit': string,
+      'valueUnitsPattern': string
+    }
+  },
+  'QuadWavelengthSelector': {
+    'Microwave': string,
+    'Infrared': string,
+    'Visible': string,
+    'Ultraviolet': string,
+    'HigherEnergy': string
+  },
+  'molecularNamePattern': string,
+  'ControlPanel': {
+    'CarbonMonoxide': string,
+    'Nitrogen': string,
+    'Oxygen': string,
+    'CarbonDioxide': string,
+    'Methane': string,
+    'NitrogenDioxide': string,
+    'Ozone': string,
+    'Water': string
+  },
+  'ButtonNode': {
+    'ReturnMolecule': string
+  },
+  'SpectrumWindow': {
+    'buttonCaption': string,
+    'title': string,
+    'frequencyArrowLabel': string,
+    'wavelengthArrowLabel': string,
+    'radioBandLabel': string,
+    'microwaveBandLabel': string,
+    'infraredBandLabel': string,
+    'ultravioletBandLabel': string,
+    'xrayBandLabel': string,
+    'gammaRayBandLabel': string,
+    'visibleBandLabel': string,
+    'cyclesPerSecondUnits': string,
+    'metersUnits': string
+  },
+  'openSciEd': {
+    'energySource': string
+  },
+  'energyBalance': string,
+  'a11y': {
+    'observationWindowLabel': string,
+    'energyBalance': {
+      'helpText': string
+    },
+    'surfaceThermometer': {
+      'helpText': string
+    },
+    'showSurfaceTemperature': {
+      'helpText': string
+    },
+    'temperatureDescriptions': {
+      'veryHigh': string,
+      'high': string,
+      'somewhatHigh': string,
+      'moderate': string,
+      'somewhatLow': string,
+      'low': string,
+      'veryLow': string
+    },
+    'temperatureUnits': {
+      'kelvin': string,
+      'celsius': string,
+      'fahrenheit': string
+    },
+    'concentrationDescriptions': {
+      'no': string,
+      'veryLow': string,
+      'low': string,
+      'moderate': string,
+      'high': string,
+      'veryHigh': string,
+      'max': string
+    },
+    'timePeriodDescriptions': {
+      'iceAge': string,
+      'seventeenFifty': string,
+      'nineteenFifty': string,
+      'twentyTwenty': string
+    },
+    'sky': {
+      'skyDescriptionPattern': string,
+      'cloudy': string,
+      'clear': string
+    },
+    'energyLegend': {
+      'title': string,
+      'inObservationWindow': string,
+      'sunlightRadiation': string,
+      'infraredRadiation': string
+    },
+    'waves': {
+      'homeScreenDescription': string,
+      'screenSummary': {
+        'playAreaDescription': string,
+        'controlAreaDescription': string,
+        'currently': string,
+        'currentlyNoSunlight': string,
+        'greenhouseGassesInAtmospherePattern': string,
+        'timePeriodPattern': string,
+        'summaryWithTemperaturePattern': string,
+        'summaryWithoutTemperaturePattern': string,
+        'surfaceTemperaturePattern': string,
+        'qualitativeAndQuantitativeTemperatureDescriptionPattern': string
+      }
+    },
+    'photons': {
+      'homeScreenDescription': string
+    },
+    'startSunlightHint': string,
+    'startSunlightButtonHelpText': string,
+    'cloudCheckboxHelpText': string,
+    'concentrationPanel': {
+      'title': string,
+      'exploreMode': string,
+      'exploreModeHelpText': string,
+      'byConcentration': string,
+      'byTimePeriod': string,
+      'timePeriod': {
+        'label': string,
+        'helpText': string,
+        'yearTwentyTwenty': string,
+        'yearNineteenFifty': string,
+        'yearSeventeenFifty': string
+      },
+      'concentration': {
+        'label': string,
+        'greenhouseGasConcentration': string,
+        'concentrationSliderHelpText': string
+      }
+    },
+    'playAreaSummary': string,
+    'controlAreaSummary': string,
+    'dynamicPlayingScreenSummaryPattern': string,
+    'dynamicPausedScreenSummaryPattern': string,
+    'simIsPaused': string,
+    'simIsPausedOnSlowSpeed': string,
+    'screenSummaryWithHintPattern': string,
+    'interactionHint': string,
+    'lightSource': {
+      'buttonLabelPattern': string,
+      'buttonPressedHelpText': string,
+      'buttonUnpressedHelpText': string
+    },
+    'lightSources': string,
+    'isOffAndPoints': string,
+    'emitsPhotons': string,
+    'emitsPhotonsOnSlowSpeed': string,
+    'photonEmitterOffDescriptionPattern': string,
+    'inactiveAndPassesPhaseDescriptionPattern': string,
+    'targetMoleculePattern': string,
+    'absorptionPhaseBondsDescriptionPattern': string,
+    'absorptionPhaseMoleculeDescriptionPattern': string,
+    'breakApartPhaseDescriptionPattern': string,
+    'emissionPhaseDescriptionPattern': string,
+    'bendUpAndDown': string,
+    'stretchBackAndForth': string,
+    'rotatesClockwise': string,
+    'rotatesCounterClockwise': string,
+    'glowsString': string,
+    'breaksApart': string,
+    'emptySpace': string,
+    'lightSourceRadioButtonHelpText': string,
+    'molecules': string,
+    'moleculesRadioButtonHelpText': string,
+    'moleculeButtonLabelPattern': string,
+    'spectrumButtonLabel': string,
+    'spectrumButtonDescription': string,
+    'spectrumWindowDescription': string,
+    'spectrumWindowEnergyDescription': string,
+    'spectrumWindowSinWaveDescription': string,
+    'spectrumWindowLabelledSpectrumLabel': string,
+    'spectrumWindowLabelledSpectrumDescription': string,
+    'spectrumWindowLabelledSpectrumRadioLabel': string,
+    'spectrumWindowLabelledSpectrumMicrowaveLabel': string,
+    'spectrumWindowLabelledSpectrumInfraredLabel': string,
+    'spectrumWindowLabelledSpectrumVisibleLabel': string,
+    'spectrumWindowLabelledSpectrumUltravioletLabel': string,
+    'spectrumWindowLabelledSpectrumXRayLabel': string,
+    'spectrumWindowLabelledSpectrumGammaRayLabel': string,
+    'spectrumWindowLabelledSpectrumRadioFrequencyDescription': string,
+    'spectrumWindowLabelledSpectrumRadioWavelengthDescription': string,
+    'spectrumWindowLabelledSpectrumMicrowaveFrequencyDescription': string,
+    'spectrumWindowLabelledSpectrumMicrowaveWavelengthDescription': string,
+    'spectrumWindowLabelledSpectrumInfraredFrequencyDescription': string,
+    'spectrumWindowLabelledSpectrumInfraredWavelengthDescription': string,
+    'spectrumWindowLabelledSpectrumVisibleFrequencyDescription': string,
+    'spectrumWindowLabelledSpectrumVisibleWavelengthDescription': string,
+    'spectrumWindowLabelledSpectrumVisibleGraphicalDescription': string,
+    'spectrumWindowLabelledSpectrumUltravioletFrequencyDescription': string,
+    'spectrumWindowLabelledSpectrumUltravioletWavelengthDescription': string,
+    'spectrumWindowLabelledSpectrumXRayFrequencyDescription': string,
+    'spectrumWindowLabelledSpectrumXRayWavelengthDescription': string,
+    'spectrumWindowLabelledSpectrumGammaRayFrequencyDescription': string,
+    'spectrumWindowLabelledSpectrumGammaRayWavelengthDescription': string,
+    'carbonMonoxide': string,
+    'nitrogen': string,
+    'oxygen': string,
+    'diatomicOxygen': string,
+    'carbonDioxide': string,
+    'nitrogenDioxide': string,
+    'water': string,
+    'ozone': string,
+    'methane': string,
+    'linear': string,
+    'bent': string,
+    'tetrahedral': string,
+    'diatomic': string,
+    'geometryLabelPattern': string,
+    'linearGeometryDescription': string,
+    'bentGeometryDescription': string,
+    'tetrahedralGeometryDescription': string,
+    'shortStretchingAlert': string,
+    'longStretchingAlert': string,
+    'shortBendingAlert': string,
+    'longBendingAlert': string,
+    'shortRotatingAlert': string,
+    'longRotatingAlert': string,
+    'shortGlowingAlert': string,
+    'longGlowingAlert': string,
+    'breaksApartAlertPattern': string,
+    'pausedEmittingPattern': string,
+    'pausedPassingPattern': string,
+    'slowMotionPassingPattern': string,
+    'photonPasses': string,
+    'photonsPassing': string,
+    'slowMotionVibratingPattern': string,
+    'slowMotionAbsorbedMoleculeExcitedPattern': string,
+    'slowMotionAbsorbedShortPattern': string,
+    'slowMotionBreakApartPattern': string,
+    'moleculesFloatingAwayPattern': string,
+    'breakApartDescriptionWithFloatPattern': string,
+    'moleculePiecesGone': string,
+    'slowMotionEmittedPattern': string,
+    'resetOrChangeMolecule': string,
+    'photonEmitter': {
+      'alerts': {
+        'photonsOff': string,
+        'photonsOn': string,
+        'photonsOnSlowSpeed': string,
+        'photonsOnSimPaused': string,
+        'photonsOnSlowSpeedSimPaused': string,
+        'pausedPhotonEmittedPattern': string
+      }
+    },
+    'timeControls': {
+      'simPausedEmitterOnAlert': string,
+      'simPausedEmitterOffAlert': string,
+      'simPlayingHintAlert': string,
+      'stepHintAlert': string
+    }
+  }
+};
+
+const greenhouseEffectStrings = getStringModule( 'GREENHOUSE_EFFECT' ) as StringsType;
+
+greenhouseEffect.register( 'greenhouseEffectStrings', greenhouseEffectStrings );
+
+export default greenhouseEffectStrings;
