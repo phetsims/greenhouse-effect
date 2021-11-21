a096210860aa8ebc16644f669d709f2dc4795cd7
jbphet <john.blanco@colorado.edu>
2021-11-20 17:04:25 -0700

    back to JS file for namespace

---
+++ js/greenhouseEffect.js
@@ -0,0 +1,10 @@
+// Copyright 2021, University of Colorado Boulder
+
+/**
+ * Creates the namespace for this simulation.
+ */
+
+// modules
+import Namespace from '../../phet-core/js/Namespace.js';
+
+export default new Namespace( 'greenhouseEffect' );
