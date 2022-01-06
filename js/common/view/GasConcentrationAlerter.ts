90afd3b5710945275d565b7bd634c232d912669a
phet-dev <phetadmin@gmail.com>
2022-01-06 03:18:12 -0700

    update copyright dates from daily grunt work

--- js/common/view/GasConcentrationAlerter.ts
+++ js/common/view/GasConcentrationAlerter.ts
@@ -1,4 +1,4 @@
-// Copyright 2021, University of Colorado Boulder
+// Copyright 2021-2022, University of Colorado Boulder
 
 /**
  * Responsible for generating and timing alerts related to gas concentration in this sim. A polling method is used
