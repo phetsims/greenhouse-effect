69f545c39d24ab741804251b07dbc97f2efb4319
phet-dev <phetadmin@gmail.com>
2022-01-15 03:25:40 -0700

    update copyright dates from daily grunt work

--- js/common/model/GroundLayer.ts
+++ js/common/model/GroundLayer.ts
@@ -1,4 +1,4 @@
-// Copyright 2021, University of Colorado Boulder
+// Copyright 2021-2022, University of Colorado Boulder
 
 /**
  * GroundLayer is a subclass of EnergyAbsorbingEmittingLayer and adds the behavior that is specific to the ground.
