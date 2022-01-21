a33e391e10882cf45c85183a0f51f33728240006
phet-dev <phetadmin@gmail.com>
2022-01-21 03:22:30 -0700

    update copyright dates from daily grunt work

--- js/common/model/Cloud.ts
+++ js/common/model/Cloud.ts
@@ -1,4 +1,4 @@
-// Copyright 2021, University of Colorado Boulder
+// Copyright 2021-2022, University of Colorado Boulder
 
 /**
  * The Cloud class is a simple model of a cloud in the atmosphere.  Clouds can interact with light, generally reflecting
