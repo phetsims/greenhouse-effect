eaae401aa0e248c635a854118385786ca2ab725f
phet-dev <phetadmin@gmail.com>
2022-02-15 03:22:28 -0700

    update copyright dates from daily grunt work

--- js/common/PhotonSpriteInstance.ts
+++ js/common/PhotonSpriteInstance.ts
@@ -1,4 +1,4 @@
-// Copyright 2020-2022, University of Colorado Boulder
+// Copyright 2022, University of Colorado Boulder
 
 /**
  * PhotonSpriteInstance is a specialization of SpriteInstance for photons.  It keeps a reference to the associated
