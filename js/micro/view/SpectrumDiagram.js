39c9214303ce70e2b1db4395a4c453775e1b5017
phet-dev <phetadmin@gmail.com>
2022-03-05 03:24:53 -0700

    update copyright dates from daily grunt work

--- js/micro/view/SpectrumDiagram.js
+++ js/micro/view/SpectrumDiagram.js
@@ -1,4 +1,4 @@
-// Copyright 2021, University of Colorado Boulder
+// Copyright 2021-2022, University of Colorado Boulder
 
 /**
  * This class defines a separate window that shows a representation of the electromagnetic spectrum.
