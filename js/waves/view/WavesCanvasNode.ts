e47eb8d7cc230e7f7ad6e236633748eb4cc7bb97
phet-dev <phetadmin@gmail.com>
2022-03-08 03:24:22 -0700

    update copyright dates from daily grunt work

--- js/waves/view/WavesCanvasNode.ts
+++ js/waves/view/WavesCanvasNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2020-2021, University of Colorado Boulder
+// Copyright 2020-2022, University of Colorado Boulder
 
 /**
  * WavesCanvasNode is a Scenery CanvasNode used to render sinusoidal waves that represent different frequencies of light
