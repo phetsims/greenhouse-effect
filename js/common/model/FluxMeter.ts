b4ea544acf9aafb107f307e915ce3cecb22aaeff
phet-dev <phetadmin@gmail.com>
2022-04-22 03:35:34 -0600

    update copyright dates from daily grunt work

--- js/common/model/FluxMeter.ts
+++ js/common/model/FluxMeter.ts
@@ -1,4 +1,4 @@
-// Copyright 2021, University of Colorado Boulder
+// Copyright 2021-2022, University of Colorado Boulder
 
 /**
  * A model component for the FluxMeter in this simulation. Contains Properties for the position
