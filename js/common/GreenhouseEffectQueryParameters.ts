3c5d16f2084db5bcd378eddc4796291de5a28380
jbphet <john.blanco@colorado.edu>
2022-04-27 12:37:35 -0600

    adjusted the default equilibrium thresholds based on designer feedback, see https://github.com/phetsims/greenhouse-effect/issues/137#issuecomment-1087882773

--- js/common/GreenhouseEffectQueryParameters.ts
+++ js/common/GreenhouseEffectQueryParameters.ts
@@ -14,13 +14,15 @@ const GreenhouseEffectQueryParameters = QueryStringMachine.getAll( {
   // meaning that the amount of incoming energy is close to the amount of outgoing energy.  There is another query
   // parameter that controls the amount of time that this must be true.  This value is in Watts per square meter.  See
   // https://github.com/phetsims/greenhouse-effect/issues/137 for more information.
-  atEquilibriumThreshold: { type: 'number', defaultValue: 0.005 },
+  // TODO: Prior to initial publication, this query parameter should be removed and the value incorporated directly into the code, see https://github.com/phetsims/greenhouse-effect/issues/137
+  atEquilibriumThreshold: { type: 'number', defaultValue: 0.004 },
 
   // This value is used in conjunction with atEquilibriumThreshold to decide whether an EnergyAbsorbingEmittingLayer is
   // in energy equilibrium.  To be considered to be in equilibrium, the net different between incoming and radiated
   // energy must be less than the threshold for the at-equilibrium time.  This value is in seconds.  See
   // https://github.com/phetsims/greenhouse-effect/issues/137 for more information.
-  atEquilibriumTime: { type: 'number', defaultValue: 1 },
+  // TODO: Prior to initial publication, this query parameter should be removed and the value incorporated directly into the code, see https://github.com/phetsims/greenhouse-effect/issues/137
+  atEquilibriumTime: { type: 'number', defaultValue: 1.5 },
 
   // a flag that starts the launches the sim with the sunlight initially started, for ease of development
   initiallyStarted: { type: 'boolean', defaultValue: false },
