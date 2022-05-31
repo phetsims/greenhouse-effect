bcc6bed351955d613a0e3a89cf6e6cb31df5f2c1
jbphet <john.blanco@colorado.edu>
2022-05-31 14:42:23 -0600

    finalized reverb sound, see https://github.com/phetsims/greenhouse-effect/issues/131

--- js/layer-model/view/SurfaceAlbedoSoundPlayer.ts
+++ js/layer-model/view/SurfaceAlbedoSoundPlayer.ts
@@ -15,10 +15,7 @@ import layerModelBaseSliderSound_mp3 from '../../../sounds/layerModelBaseSliderS
 import SoundClip from '../../../../tambo/js/sound-generators/SoundClip.js';
 import ISoundPlayer from '../../../../tambo/js/ISoundPlayer.js';
 import phetAudioContext from '../../../../tambo/js/phetAudioContext.js';
-import impulseResponseLargeHall_mp3 from '../../../../tambo/sounds/impulseResponseLargeHall_mp3.js';
-import impulseResponseLargeRoom_mp3 from '../../../../tambo/sounds/impulseResponseLargeRoom_mp3.js';
-
-const USE_HALL = true;
+import emptyApartmentBedroom06Resampled_mp3 from '../../../../tambo/sounds/emptyApartmentBedroom06Resampled_mp3.js';
 
 // types for options
 type SurfaceAlbedoSoundPlayerSelfOptions = {};
@@ -54,9 +51,7 @@ class SurfaceAlbedoSoundPlayer extends SoundGenerator implements ISoundPlayer {
 
     // Add a convolver that will act as a reverb effect.
     const convolver = phetAudioContext.createConvolver();
-    convolver.buffer = USE_HALL ?
-                       impulseResponseLargeHall_mp3.audioBufferProperty.value :
-                       impulseResponseLargeRoom_mp3.audioBufferProperty.value;
+    convolver.buffer = emptyApartmentBedroom06Resampled_mp3.audioBufferProperty.value;
 
     // Add a gain node that will be used for the reverb level.
     const reverbGainNode = phetAudioContext.createGain();
