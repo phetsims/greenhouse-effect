13aff7d3d342c8774b3b7e10461415544b8f8ba1
phet-dev <phetadmin@gmail.com>
2022-09-15 05:06:27 -0600

    update copyright dates from daily grunt work

--- js/layer-model/view/ShowTemperatureCheckbox.ts
+++ js/layer-model/view/ShowTemperatureCheckbox.ts
@@ -1,4 +1,4 @@
-// Copyright 2021-2022, University of Colorado Boulder
+// Copyright 2022, University of Colorado Boulder
 
 /**
  * ShowTemperatureCheckbox is a specialized checkbox with a thermometer icon and no label.
