8e37b9ffb48a1278ede8d7932f31a08d51d7c509
jbphet <jpblanco@yahoo.com>
2022-09-23 16:46:51 -0600

    remove the "Micro" screen, see https://github.com/phetsims/greenhouse-effect/issues/214

--- js/greenhouse-effect-main.ts
+++ js/greenhouse-effect-main.ts
@@ -6,6 +6,7 @@
  * @author John Blanco
  */
 
+import PreferencesModel from '../../joist/js/preferences/PreferencesModel.js';
 import Sim, { SimOptions } from '../../joist/js/Sim.js';
 import simLauncher from '../../joist/js/simLauncher.js';
 import Tandem from '../../tandem/js/Tandem.js';
@@ -13,10 +14,8 @@ import GreenhouseEffectQueryParameters from './common/GreenhouseEffectQueryParam
 import GreenhouseEffectPreferencesContent from './common/view/GreenhouseEffectPreferencesContent.js';
 import GreenhouseEffectStrings from './GreenhouseEffectStrings.js';
 import LayerModelScreen from './layer-model/LayerModelScreen.js';
-import MicroScreen from './micro/MicroScreen.js';
 import PhotonsScreen from './photons/PhotonsScreen.js';
 import WavesScreen from './waves/WavesScreen.js';
-import PreferencesModel from '../../joist/js/preferences/PreferencesModel.js';
 
 const greenhouseEffectTitleStringProperty = GreenhouseEffectStrings[ 'greenhouse-effect' ].titleStringProperty;
 
@@ -62,10 +61,14 @@ if ( GreenhouseEffectQueryParameters.customPreferences ) {
 // launch the sim - beware that scenery Image nodes created outside of simLauncher.launch() will have zero bounds
 // until the images are fully loaded, see https://github.com/phetsims/coulombs-law/issues/70
 simLauncher.launch( () => {
-  const sim = new Sim( greenhouseEffectTitleStringProperty, [
-    new WavesScreen( Tandem.ROOT.createTandem( 'wavesScreen' ) ),
-    new PhotonsScreen( Tandem.ROOT.createTandem( 'photonsScreen' ) ),
-    new LayerModelScreen( Tandem.ROOT.createTandem( 'layerModelScreen' ) ),
-    new MicroScreen( Tandem.ROOT.createTandem( 'microScreen' ) ) ], simOptions );
+  const sim = new Sim(
+    greenhouseEffectTitleStringProperty,
+    [
+      new WavesScreen( Tandem.ROOT.createTandem( 'wavesScreen' ) ),
+      new PhotonsScreen( Tandem.ROOT.createTandem( 'photonsScreen' ) ),
+      new LayerModelScreen( Tandem.ROOT.createTandem( 'layerModelScreen' ) )
+    ],
+    simOptions
+  );
   sim.start();
 } );
