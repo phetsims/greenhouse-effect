8fb0120efd3fbdea22a804f2fa2948b06580af69
Sam Reid <reids@colorado.edu>
2022-09-28 15:36:05 -0600

    Revert "Improve types, see https://github.com/phetsims/tandem/issues/263"
    This reverts commit 68266150e8a77dd34adb9380e350d8c71cba0945.

--- js/common/model/PhotonCollection.ts
+++ js/common/model/PhotonCollection.ts
@@ -284,9 +284,11 @@ class PhotonCollection extends PhetioObject {
     );
   }
 
-  public static STATE_SCHEMA: Record<string, IOType> = {
-    photonStateObjects: ArrayIO( Photon.PhotonIO )
-  };
+  public static STATE_SCHEMA(): Record<string, IOType> {
+    return {
+      photonStateObjects: ArrayIO( Photon.PhotonIO )
+    };
+  }
 
   /**
    * Restore initial state.
