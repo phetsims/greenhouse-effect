9b542d3488995a50d2ad0c307f8989eb17fdbaa1
jbphet <jpblanco@yahoo.com>
2022-12-23 14:02:55 -0700

    don't include flux meter info on Waves screen, see https://github.com/phetsims/greenhouse-effect/issues/180

--- js/common/view/GreenhouseEffectKeyboardHelpContent.ts
+++ js/common/view/GreenhouseEffectKeyboardHelpContent.ts
@@ -5,29 +5,31 @@
  * @author Jesse Greenberg (PhET Interactive Simulations)
  */
 
-import StringProperty from '../../../../axon/js/StringProperty.js';
+import optionize from '../../../../phet-core/js/optionize.js';
 import BasicActionsKeyboardHelpSection from '../../../../scenery-phet/js/keyboard/help/BasicActionsKeyboardHelpSection.js';
 import KeyboardHelpSection from '../../../../scenery-phet/js/keyboard/help/KeyboardHelpSection.js';
-import SliderControlsKeyboardHelpSection from '../../../../scenery-phet/js/keyboard/help/SliderControlsKeyboardHelpSection.js';
+import SliderControlsKeyboardHelpSection, { SliderControlsKeyboardHelpSectionOptions } from '../../../../scenery-phet/js/keyboard/help/SliderControlsKeyboardHelpSection.js';
 import TimingControlsKeyboardHelpSection from '../../../../scenery-phet/js/keyboard/help/TimingControlsKeyboardHelpSection.js';
-import TwoColumnKeyboardHelpContent from '../../../../scenery-phet/js/keyboard/help/TwoColumnKeyboardHelpContent.js';
+import TwoColumnKeyboardHelpContent, { TwoColumnKeyboardHelpContentOptions } from '../../../../scenery-phet/js/keyboard/help/TwoColumnKeyboardHelpContent.js';
 import greenhouseEffect from '../../greenhouseEffect.js';
-import GreenhouseEffectStrings from '../../GreenhouseEffectStrings.js';
+
+type SelfOptions = {
+  sliderHelpSectionOptions?: SliderControlsKeyboardHelpSectionOptions;
+};
+export type GreenhouseEffectKeyboardHelpContentOptions = SelfOptions & TwoColumnKeyboardHelpContentOptions;
 
 class GreenhouseEffectKeyboardHelpContent extends TwoColumnKeyboardHelpContent {
-  public constructor() {
-    const sliderHelpSection = new SliderControlsKeyboardHelpSection( {
-      headingStringProperty: GreenhouseEffectStrings.sliderAndFluxMeterControlsStringProperty,
+  public constructor( providedOptions?: GreenhouseEffectKeyboardHelpContentOptions ) {
 
-      // The following option essentially removes the word "slider" so that the dialog just says "Adjust" and not
-      // "Adjust Slider".  By making it a fixed property, we are not allowing it to be translated, which is intentional.
-      sliderStringProperty: new StringProperty( '' )
-    } );
+    const options = optionize<GreenhouseEffectKeyboardHelpContentOptions, SelfOptions, TwoColumnKeyboardHelpContent>()( {
+      sliderHelpSectionOptions: {}
+    }, providedOptions );
+
+    /* Create the various sections that will be combined to make up the dialog contents. */const sliderHelpSection = new SliderControlsKeyboardHelpSection( options.sliderHelpSectionOptions );
     const basicActionsHelpSection = new BasicActionsKeyboardHelpSection();
-
     const timingControlsHelpSection = new TimingControlsKeyboardHelpSection();
 
-    // vertically align the left sections
+    // Vertically align the left sections.
     KeyboardHelpSection.alignHelpSectionIcons( [ sliderHelpSection, timingControlsHelpSection ] );
 
     super( [ sliderHelpSection, timingControlsHelpSection ], [ basicActionsHelpSection ] );
