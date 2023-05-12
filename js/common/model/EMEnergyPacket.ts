23b532f55138abde83094ae4e9edffe44f81a5ed
phet-dev <phetadmin@gmail.com>
2023-05-12 03:58:11 -0600

    update copyright dates from daily grunt work

--- js/common/model/EMEnergyPacket.ts
+++ js/common/model/EMEnergyPacket.ts
@@ -1,4 +1,4 @@
-// Copyright 2021-2022, University of Colorado Boulder
+// Copyright 2021-2023, University of Colorado Boulder
 
 /**
  * EMEnergyPacket models a packet or bundle of electromagnetic energy.  It's kind of like a really big photon.
