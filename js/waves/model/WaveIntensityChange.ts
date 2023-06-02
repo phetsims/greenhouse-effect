e11ed8437da3b49065b5f938e7a87c1761d84210
phet-dev <phetadmin@gmail.com>
2023-06-02 03:29:58 -0600

    update copyright dates from daily grunt work

--- js/waves/model/WaveIntensityChange.ts
+++ js/waves/model/WaveIntensityChange.ts
@@ -1,4 +1,4 @@
-// Copyright 2022, University of Colorado Boulder
+// Copyright 2022-2023, University of Colorado Boulder
 
 import PhetioObject from '../../../../tandem/js/PhetioObject.js';
 import IOType from '../../../../tandem/js/types/IOType.js';
