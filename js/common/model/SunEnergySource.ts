a72ffadf12cd8cca422c3f7a41e07e1a23fe0b81
phet-dev <phetadmin@gmail.com>
2023-06-13 03:30:14 -0600

    update copyright dates from daily grunt work

--- js/common/model/SunEnergySource.ts
+++ js/common/model/SunEnergySource.ts
@@ -1,4 +1,4 @@
-// Copyright 2021-2022, University of Colorado Boulder
+// Copyright 2021-2023, University of Colorado Boulder
 
 /**
  * SunEnergySource is used to produce energy at a constant rate.  The amount of energy produced is based on what the
