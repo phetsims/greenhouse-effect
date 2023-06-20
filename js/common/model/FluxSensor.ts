a2b43285d465d2e2e59b5792b267856a0d235fda
phet-dev <phetadmin@gmail.com>
2023-06-20 03:31:37 -0600

    update copyright dates from daily grunt work

--- js/common/model/FluxSensor.ts
+++ js/common/model/FluxSensor.ts
@@ -1,4 +1,4 @@
-// Copyright 2022, University of Colorado Boulder
+// Copyright 2022-2023, University of Colorado Boulder
 
 /**
  * FluxSensor is a two-dimensional sensor which is modeled as a rectangle that is parallel to the ground and measures
