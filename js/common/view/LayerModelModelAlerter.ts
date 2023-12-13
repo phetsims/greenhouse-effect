62701673bb351867b91e702ddc872103c4587cac
jbphet <jpblanco@yahoo.com>
2023-12-12 17:15:24 -0700

    add basic infrastructure for periodic alerts, see https://github.com/phetsims/greenhouse-effect/issues/374

--- js/common/view/LayerModelModelAlerter.ts
+++ js/common/view/LayerModelModelAlerter.ts
@@ -15,10 +15,22 @@ import LayersModelAlerter, { LayersModelAlerterOptions } from './LayersModelAler
 type SelfOptions = EmptySelfOptions;
 export type LayerModelModelAlerterOptions = SelfOptions & LayersModelAlerterOptions;
 
+// The parts of the Layer Model model that are monitored for changes over time and that can motivate alerts.
+type LayerModelModelState = {
+  solarIntensity: number;
+  surfaceAlbedo: number;
+  numberOfAbsorbingLayers: number;
+  infraredAbsorbance: number;
+};
+
 class LayerModelModelAlerter extends LayersModelAlerter {
 
   // reference to the model, used in the methods
   private readonly layerModelModel: LayerModelModel;
+
+  // Elements of the model state that are specific to the model from the Layer Model screen that are monitored for
+  // changes over time to determine if alerts are needed.
+  private previousPeriodicNotificationState: LayerModelModelState;
 
   public constructor( model: LayerModelModel, providedOptions: LayerModelModelAlerterOptions ) {
 
@@ -31,15 +43,42 @@ class LayerModelModelAlerter extends LayersModelAlerter {
     super( model, options );
 
     this.layerModelModel = model;
+    this.previousPeriodicNotificationState = this.saveLayerModelPeriodicNotificationState();
+  }
+
+  private saveLayerModelPeriodicNotificationState(): LayerModelModelState {
+    this.previousPeriodicNotificationState = this.previousPeriodicNotificationState || {
+      solarIntensity: 0,
+      surfaceAlbedo: 0,
+      numberOfAbsorbingLayers: 0,
+      infraredAbsorbance: 0
+    };
+
+    this.previousPeriodicNotificationState.solarIntensity = this.layerModelModel.sunEnergySource.proportionateOutputRateProperty.value;
+    this.previousPeriodicNotificationState.surfaceAlbedo = this.layerModelModel.groundLayer.albedoProperty.value;
+    this.previousPeriodicNotificationState.numberOfAbsorbingLayers = this.layerModelModel.numberOfActiveAtmosphereLayersProperty.value;
+    this.previousPeriodicNotificationState.infraredAbsorbance = this.layerModelModel.layersInfraredAbsorbanceProperty.value;
+
+    return this.previousPeriodicNotificationState;
   }
 
   /**
-   * Step this alerter.  See base class for more documentation about this.
+   * Check if anything has changed in this model that deserves an alert that isn't handled by the base class.
    */
-  public override step(): void {
-    super.step();
+  protected override checkAndPerformPeriodicAlerts(): void {
+    super.checkAndPerformPeriodicAlerts();
 
-    // TODO: Add specific alerts for this class, see https://github.com/phetsims/greenhouse-effect/issues/374.
+    const doIrAlert = this.previousPeriodicNotificationState.solarIntensity !== this.layerModelModel.sunEnergySource.proportionateOutputRateProperty.value ||
+                      this.previousPeriodicNotificationState.surfaceAlbedo !== this.layerModelModel.groundLayer.albedoProperty.value ||
+                      this.previousPeriodicNotificationState.numberOfAbsorbingLayers !== this.layerModelModel.numberOfActiveAtmosphereLayersProperty.value ||
+                      this.previousPeriodicNotificationState.infraredAbsorbance !== this.layerModelModel.layersInfraredAbsorbanceProperty.value;
+
+    if ( doIrAlert && this.layerModelModel.isInfraredPresent() ) {
+      this.alert( 'Some sort of IR thing changed.' );
+    }
+
+    // Save state for the next round.
+    this.saveLayerModelPeriodicNotificationState();
   }
 }
 
