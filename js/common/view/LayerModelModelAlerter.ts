07195533a506f94198177217f9be8c6d67b20c1d
Jesse <jesse.greenberg@colorado.edu>
2023-12-15 19:02:00 -0500

    check for periodic energy balance alerts in the Layer Model screen, see #374

--- js/common/view/LayerModelModelAlerter.ts
+++ js/common/view/LayerModelModelAlerter.ts
@@ -113,6 +113,11 @@ class LayerModelModelAlerter extends LayersModelAlerter {
     return this.previousImmediateNotificationState;
   }
 
+  protected override checkAndPerformPeriodicAlerts(): void {
+    this.checkAndPerformEnergyBalanceAlerts();
+    super.checkAndPerformPeriodicAlerts();
+  }
+
   /**
    * Check if anything has changed in this model that deserves an alert that isn't handled by the base class.
    */
