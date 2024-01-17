580128f1753e3d9d25b33f1a5445d76dca904b6d
phet-dev <phetadmin@gmail.com>
2024-01-17 03:14:18 -0700

    update copyright dates from daily grunt work

--- js/common/view/describers/TemperatureDescriber.ts
+++ js/common/view/describers/TemperatureDescriber.ts
@@ -1,4 +1,4 @@
-// Copyright 2021-2023, University of Colorado Boulder
+// Copyright 2021-2024, University of Colorado Boulder
 
 /**
  * Responsible for generating description strings that describe the temperature.
