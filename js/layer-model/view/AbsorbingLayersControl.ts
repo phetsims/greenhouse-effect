2489477c4813c363755375deb3f91dc5c1e91858
jbphet <jpblanco@yahoo.com>
2024-02-07 12:12:29 -0600

    remove redundant alert code, see https://github.com/phetsims/greenhouse-effect/issues/386

--- js/layer-model/view/AbsorbingLayersControl.ts
+++ js/layer-model/view/AbsorbingLayersControl.ts
@@ -18,7 +18,6 @@ import GreenhouseEffectConstants from '../../common/GreenhouseEffectConstants.js
 import NumberPicker from '../../../../sun/js/NumberPicker.js';
 import PhetFont from '../../../../scenery-phet/js/PhetFont.js';
 import GreenhouseEffectColors from '../../common/GreenhouseEffectColors.js';
-import StringUtils from '../../../../phetcommon/js/util/StringUtils.js';
 
 export default class AbsorbingLayersControl extends HBox {
 
@@ -50,53 +49,7 @@ export default class AbsorbingLayersControl extends HBox {
         labelContent: GreenhouseEffectStrings.absorbingLayersStringProperty,
         helpText: GreenhouseEffectStrings.a11y.layerModel.absorbingLayersHelpTextStringProperty,
         labelTagName: 'label',
-        pageKeyboardStep: 1, // page up/page down have same size as default step
-
-        // Creates a context response that describes the change in the number of layers. Will return something like:
-        // 'Layer 2 added above layer 1.` or
-        // 'Layer 1 removed.'
-        a11yCreateContextResponseAlert: ( mappedValue, value, previousValue ) => {
-          let response = '';
-          if ( previousValue !== null && value !== previousValue ) {
-            const difference = Math.abs( value - previousValue );
-
-            if ( value > previousValue ) {
-
-              if ( difference > 1 ) {
-                response = GreenhouseEffectStrings.a11y.layerModel.observationWindow.multipleLayersAddedStringProperty.value;
-              }
-              else if ( value === 1 ) {
-
-                // describe relative to ground layer
-                response = StringUtils.fillIn(
-                  GreenhouseEffectStrings.a11y.layerModel.observationWindow.layerAddedAboveSurfacePatternStringProperty,
-                  { number: value }
-                );
-              }
-              else {
-                response = StringUtils.fillIn(
-                  GreenhouseEffectStrings.a11y.layerModel.observationWindow.layerAddedContextResponsePatternStringProperty,
-                  {
-                    aboveNumber: value,
-                    belowNumber: previousValue
-                  }
-                );
-              }
-            }
-            else {
-              if ( difference > 1 ) {
-                response = GreenhouseEffectStrings.a11y.layerModel.observationWindow.multipleLayersRemovedStringProperty.value;
-              }
-              else {
-                response = StringUtils.fillIn(
-                  GreenhouseEffectStrings.a11y.layerModel.observationWindow.layerRemovedContextResponsePatternStringProperty,
-                  { number: previousValue }
-                );
-              }
-            }
-          }
-          return response;
-        }
+        pageKeyboardStep: 1 // page up/page down have same size as default step
       }
     );
 
