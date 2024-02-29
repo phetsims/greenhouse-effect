72a9f53ce72962d891cdf7643547126fde960a15
phet-dev <phetadmin@gmail.com>
2024-02-29 03:18:46 -0700

    update copyright dates from daily grunt work

--- js/common/view/ThermometerAndReadout.ts
+++ js/common/view/ThermometerAndReadout.ts
@@ -1,4 +1,4 @@
-// Copyright 2021-2023, University of Colorado Boulder
+// Copyright 2021-2024, University of Colorado Boulder
 
 /**
  * ThermometerAndReadout is a Scenery Node that depicts a thermometer and a numerical readout that indicates the
