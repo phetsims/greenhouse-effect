19cb38dc987f9cef6093060b261b3e53ecc0fd1a
jbphet <jpblanco@yahoo.com>
2024-05-29 13:52:37 -0600

    update credits, see https://github.com/phetsims/greenhouse-effect/issues/414

--- js/greenhouse-effect-main.ts
+++ js/greenhouse-effect-main.ts
@@ -26,11 +26,14 @@ const simOptions: SimOptions = {
   credits: {
     leadDesign: 'Kathy Perkins, Amy Rouinfar',
     softwareDevelopment: 'John Blanco, Jesse Greenberg, Sam Reid',
-    team: 'Wendy Adams, Wanda D\u00edaz Merced, Anne U. Gold, Danielle Harlow, Kelly Lancaster, <br>Trish Loeblein, Matthew Moore, Ariel Paul, Robert Parson, Taliesin Smith, Carl Wieman',
-    qualityAssurance: 'Jaron Droder, Clifford Hardin, Amanda McGarry, Emily Miller, Nancy Salpepi,<br>Marla Schulz, Luisa Vargas, Kathryn Woessner',
+    team: 'Wendy Adams, Wanda D\u00edaz Merced, Anne U. Gold, Danielle Harlow, Kelly Lancaster,<br>' +
+          'Trish Loeblein, Matthew Moore, Mark Nielsen, Ariel Paul, Robert Parson, Taliesin Smith, Carl Wieman',
+    qualityAssurance: 'Jaron Droder, Clifford Hardin, Amanda McGarry, Emily Miller, Nancy Salpepi,<br>Marla Schulz, ' +
+                      'Luisa Vargas, Kathryn Woessner',
     graphicArts: 'Mariah Hermsmeyer',
     soundDesign: 'Ashton Morris',
-    thanks: 'Dedicated to the memory of Ron LeMaster.'
+    thanks: 'HHMI BioInteractive for their partnership on the inclusive design and accessibility of this simulation.<br>' +
+            'Dedicated to the memory of Ron LeMaster.'
   },
   preferencesModel: new PreferencesModel( {
     simulationOptions: {
