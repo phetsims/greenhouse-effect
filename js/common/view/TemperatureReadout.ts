80ae67ae3154faf25a385cae0782bcaf569fb4bf
phet-dev <phetadmin@gmail.com>
2024-05-30 03:16:23 -0600

    update copyright dates from daily grunt work

--- js/common/view/TemperatureReadout.ts
+++ js/common/view/TemperatureReadout.ts
@@ -1,4 +1,4 @@
-// Copyright 2021-2024, University of Colorado Boulder
+// Copyright 2024, University of Colorado Boulder
 
 /**
  * TemperatureReadout is a node for displaying temperature values with units in a consistent way.
