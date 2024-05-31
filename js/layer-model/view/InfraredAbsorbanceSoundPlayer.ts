c09d9cfe5c87bbfb44d916cae5c3b4b45251a3a5
phet-dev <phetadmin@gmail.com>
2024-05-31 03:15:58 -0600

    update copyright dates from daily grunt work

--- js/layer-model/view/InfraredAbsorbanceSoundPlayer.ts
+++ js/layer-model/view/InfraredAbsorbanceSoundPlayer.ts
@@ -1,4 +1,4 @@
-// Copyright 2022-2023, University of Colorado Boulder
+// Copyright 2022-2024, University of Colorado Boulder
 
 /**
  * IrAbsorbanceSoundPlayer is used to produce sounds for the slider that controls the absorbance of a set of IR
