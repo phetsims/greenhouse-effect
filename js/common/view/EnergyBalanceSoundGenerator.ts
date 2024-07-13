588ef6efa45a3024f224ef692c7d500bc3bbdd9a
phet-dev <phetadmin@gmail.com>
2024-07-13 04:07:11 -0600

    update copyright dates from daily grunt work

--- js/common/view/EnergyBalanceSoundGenerator.ts
+++ js/common/view/EnergyBalanceSoundGenerator.ts
@@ -1,4 +1,4 @@
-// Copyright 2022-2023, University of Colorado Boulder
+// Copyright 2022-2024, University of Colorado Boulder
 
 /**
  * EnergyBalanceSoundGenerator is used to produce sounds that represent the balance of energy at the top of the
