d7bd8b6c46ba4cca9f6aec601821ad04e336555f
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-09-26 14:09:48 -0600

    typo, https://github.com/phetsims/chipper/issues/1474
    Signed-off-by: Michael Kauzmann <michael.kauzmann@colorado.edu>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 // Copyright 2024, University of Colorado Boulder
 
 /**
- * ESlint configuration for greenhouse-effect.
+ * ESLint configuration for greenhouse-effect.
  *
  * @author Sam Reid (PhET Interactive Simulations)
  * @author Michael Kauzmann (PhET Interactive Simulations)
