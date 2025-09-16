898ac7c56da8b08b192d934602fd38857226be78
Jesse <jesse.greenberg@colorado.edu>
2025-09-16 17:29:53 -0400

    ts conversion, see #423

--- js/micro/model/molecules/N2.ts
+++ js/micro/model/molecules/N2.ts
@@ -1,7 +1,7 @@
 // Copyright 2021, University of Colorado Boulder
 
 /**
- * Class that represents N2 ( nitrogen ) in the model.
+ * Class that represents N2 (nitrogen) in the model.
  *
  * @author John Blanco
  * @author Jesse Greenberg
@@ -11,27 +11,23 @@ import Vector2 from '../../../../../dot/js/Vector2.js';
 import greenhouseEffect from '../../../greenhouseEffect.js';
 import Atom from '../atoms/Atom.js';
 import AtomicBond from '../atoms/AtomicBond.js';
-import Molecule from '../Molecule.js';
+import Molecule, { MoleculeOptions } from '../Molecule.js';
 
 // Model data for nitrogen molecule
 const INITIAL_NITROGEN_NITROGEN_DISTANCE = 170; // In picometers.
 
 class N2 extends Molecule {
 
+  private readonly nitrogenAtom1 = Atom.nitrogen();
+  private readonly nitrogenAtom2 = Atom.nitrogen();
+
   /**
    * Constructor for a molecule of nitrogen.
-   *
-   * @param {Object} [options]
    */
-  constructor( options ) {
+  public constructor( options?: MoleculeOptions ) {
 
     // Supertype constructor
     super( options );
-
-    // Instance data for the nitrogen molecule
-    // @private
-    this.nitrogenAtom1 = Atom.nitrogen();
-    this.nitrogenAtom2 = Atom.nitrogen();
 
     // Configure the base class.
     this.addAtom( this.nitrogenAtom1 );
@@ -40,20 +36,16 @@ class N2 extends Molecule {
 
     // Set the initial offsets
     this.initializeAtomOffsets();
-
   }
-
 
   /**
    * Initialize and set the center of gravity offsets for the nitrogen atoms which compose this molecule.
-   * @private
    */
-  initializeAtomOffsets() {
+  protected override initializeAtomOffsets(): void {
 
     this.addInitialAtomCogOffset( this.nitrogenAtom1, new Vector2( -INITIAL_NITROGEN_NITROGEN_DISTANCE / 2, 0 ) );
     this.addInitialAtomCogOffset( this.nitrogenAtom2, new Vector2( INITIAL_NITROGEN_NITROGEN_DISTANCE / 2, 0 ) );
     this.updateAtomPositions();
-
   }
 }
 
