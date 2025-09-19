b17c00bf28e7053ad95fdf3929bc9188a03b56c5
Jesse <jesse.greenberg@colorado.edu>
2025-09-19 17:38:37 -0400

    typescript conversion, see #423

--- js/micro/view/MicroPhotonNode.ts
+++ js/micro/view/MicroPhotonNode.ts
@@ -7,6 +7,7 @@
  * @author Jesse Greenberg
  */
 
+import ModelViewTransform2 from '../../../../phetcommon/js/view/ModelViewTransform2.js';
 import Image from '../../../../scenery/js/nodes/Image.js';
 import Node from '../../../../scenery/js/nodes/Node.js';
 import infraredPhoton_png from '../../../images/infraredPhoton_png.js';
@@ -14,10 +15,11 @@ import microwavePhoton_png from '../../../images/microwavePhoton_png.js';
 import ultravioletPhoton_png from '../../../images/ultravioletPhoton_png.js';
 import visiblePhoton_png from '../../../images/visiblePhoton_png.js';
 import greenhouseEffect from '../../greenhouseEffect.js';
+import MicroPhoton from '../model/MicroPhoton.js';
 import WavelengthConstants from '../model/WavelengthConstants.js';
 
 // Map of photon wavelengths to visual images used for representing them.
-const mapWavelengthToImageName = {};
+const mapWavelengthToImageName: Record<number, HTMLImageElement> = {};
 mapWavelengthToImageName[ WavelengthConstants.MICRO_WAVELENGTH ] = microwavePhoton_png;
 mapWavelengthToImageName[ WavelengthConstants.IR_WAVELENGTH ] = infraredPhoton_png;
 mapWavelengthToImageName[ WavelengthConstants.VISIBLE_WAVELENGTH ] = visiblePhoton_png;
@@ -25,20 +27,15 @@ mapWavelengthToImageName[ WavelengthConstants.UV_WAVELENGTH ] = ultravioletPhoto
 
 class MicroPhotonNode extends Node {
 
+  private readonly photon: MicroPhoton;
+  private readonly modelViewTransform: ModelViewTransform2;
+
   /**
    * Constructor for a photon node.
-   *
-   * @param {Photon} photon
-   * @param {ModelViewTransform2} modelViewTransform
    */
-  constructor( photon, modelViewTransform ) {
-
-    // supertype constructor
+  public constructor( photon: MicroPhoton, modelViewTransform: ModelViewTransform2 ) {
     super();
 
-    // Carry this node through the scope in nested functions.
-
-    // @private
     this.photon = photon;
     this.modelViewTransform = modelViewTransform;
 
