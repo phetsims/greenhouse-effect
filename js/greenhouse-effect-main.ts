e0845542ef8922db36afbc0eeb8da4d371a603da
AgustinVallejo <agustin.vallejo@udea.edu.co>
2026-04-17 17:32:22 -0500

    sync-to-poly: Adding unicode accents to credits instead of escape characters

--- js/greenhouse-effect-main.ts
+++ js/greenhouse-effect-main.ts
@@ -26,7 +26,7 @@ const simOptions: SimOptions = {
   credits: {
     leadDesign: 'Kathy Perkins, Amy Rouinfar',
     softwareDevelopment: 'John Blanco, Jesse Greenberg, Sam Reid',
-    team: 'Wendy Adams, Wanda D\u00edaz Merced, Anne U. Gold, Danielle Harlow, Kelly Lancaster,<br>' +
+    team: 'Wendy Adams, Wanda Díaz Merced, Anne U. Gold, Danielle Harlow, Kelly Lancaster,<br>' +
           'Trish Loeblein, Matthew Moore, Mark Nielsen, Ariel Paul, Robert Parson, Taliesin Smith, Carl Wieman',
     qualityAssurance: 'Jaron Droder, Clifford Hardin, Amanda McGarry, Emily Miller, Nancy Salpepi,<br>Marla Schulz, ' +
                       'Luisa Vargas, Kathryn Woessner',
